13145e2669e7fede4afc06e16613ea1ca4968f24
Komla Adzam <adzamkomla.dev@gmail.com>
2024-08-17 20:01:52 +0000

    feat: draw up structure

---
+++ app/Actions/Audiences/Index.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions\Audiences;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class Index
+{
+    use AsAction;
+
+    public function handle()
+    {
+        // ...
+    }
+}
---
+++ app/Actions/Coalations/Index.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions\Coalations;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class Index
+{
+    use AsAction;
+
+    public function handle()
+    {
+        // ...
+    }
+}
