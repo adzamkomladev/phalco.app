bc295e425b97bae6a20160ea7df7474749cb7508
Komla Adzam <adzamkomla.dev@gmail.com>
2024-08-17 19:41:37 +0000

    feat: add reverb

--- resources/application/main.ts
+++ resources/application/main.ts
@@ -3,15 +3,16 @@ import { createHead, useHead } from '@unhead/vue'
 import i18n from './i18n'
 import './tailwind.css'
 import "preline/preline";
+import "./../js/echo.js";
 
 
 initializeHybridly({
-	enhanceVue: (vue) => {
-		const head = createHead();
-		head.push({
+    enhanceVue: (vue) => {
+        const head = createHead();
+        head.push({
             titleTemplate: (title) => title ? `${title} — Phalco` : 'Phalco',
-		});
-		vue.use(i18n)
-		vue.use(head);
-	},
+        });
+        vue.use(i18n)
+        vue.use(head);
+    },
 })
