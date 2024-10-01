e6fb8b3c44afbe1adce89921219e11d44de5df6f
Komla Adzam <adzamkomla.dev@gmail.com>
2024-10-01 00:04:41 +0000

    feat: create actions for agents

---
+++ app/Actions/Voting/Agents/Upload.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions\Voting\Agents;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class Upload
+{
+    use AsAction;
+
+    public function handle()
+    {
+        // ...
+    }
+}
