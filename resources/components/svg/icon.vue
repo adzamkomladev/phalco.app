94d6cc20bdbbc46db7bd829aaa12caffecbc03a5
EveryJE <kgyan19lf@outlook.com>
2024-10-15 15:32:19 +0100

    chore: code formatted

--- resources/components/svg/icon.vue
+++ resources/components/svg/icon.vue
@@ -1,28 +1,27 @@
 <script setup lang="ts">
-
 const props = withDefaults(
-  defineProps<{
-    name?: string; 
-    size?: number;
-    color?: string;
-  }>(),
-  { size: 24, color: "#000", name: "diamond" }
+    defineProps<{
+        name?: string;
+        size?: number;
+        color?: string;
+    }>(),
+    { size: 24, color: "#000", name: "diamond" },
 );
 
 const Icon = ref(null);
 
 watchEffect(async () => {
-  if (props.name) {
-    Icon.value = (await import(`./icon/${props.name}.vue`)).default;
-  }
+    if (props.name) {
+        Icon.value = (await import(`./icon/${props.name}.vue`)).default;
+    }
 });
 </script>
 
 <template>
-  <component
-    v-if="Icon"
-    :is="Icon"
-    :style="{ height: size + 'px', color: color }"
-  />
-  <p v-else>Icon not found</p>
+    <component
+        v-if="Icon"
+        :is="Icon"
+        :style="{ height: size + 'px', color: color }"
+    />
+    <p v-else>Icon not found</p>
 </template>
