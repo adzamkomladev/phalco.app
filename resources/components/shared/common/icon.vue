8228ed595d1348e38e8d04fe42aab7a7267b7303
Komla Adzam <adzamkomla.dev@gmail.com>
2024-10-16 10:50:16 +0000

    fix: svg imports

---
+++ resources/components/shared/common/icon.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = withDefaults(
+    defineProps<{
+        name?: string;
+        size?: number;
+        color?: string;
+    }>(),
+    { size: 24, color: "#000", name: "diamond" },
+);
+
+let Icon = ref(null);
+
+onMounted(async () => {
+    Icon.value = await import(`../../../svg/icons/${props.name}.svg?component`);
+});
+</script>
+
+<template>
+    <Icon v-if="Icon" :height="size" :width="size" :style="{color}" />
+    <p v-else></p>
+</template>
