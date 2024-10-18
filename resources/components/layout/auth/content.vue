4f2197888a1c91c2353067de961a7d1b5e5656dd
EveryJE <kgyan19lf@outlook.com>
2024-10-18 18:58:08 +0100

    auth:fixed layout alignment issues

--- resources/components/layout/auth/content.vue
+++ resources/components/layout/auth/content.vue
@@ -8,7 +8,9 @@ const props = defineProps<{
 
 <template>
     <div class="self-end portrait:pt-0 tall:pt-8 xtall:pt-16 md:text-sm">
-        <div class="text-center sm:landscape:text-left pb-6  portrait:flex flex-col items-center">
+        <div
+            class="text-center sm:landscape:text-left pb-6 portrait:flex flex-col items-center"
+        >
             <p
                 class="text-black dark:text-primary-700 font-black text-2xl _md:landscape:text-2xl sm:text-3xl 2xl:text-4xl"
             >
@@ -16,17 +18,20 @@ const props = defineProps<{
                 <slot v-else name="title" />
             </p>
             <div>
-
-          
-            <p class="text-gray-500 portrait:max-w-[22rem] max-w-[28rem] block text-wrap text-base md:text-lg ">
-                <span v-if="description">{{ description }}</span>
-                <slot v-else name="description" />
-            </p>  
-        </div>
+                <p
+                    class="text-gray-500 portrait:max-w-[22rem] max-w-[28rem] block text-wrap text-base md:text-lg"
+                >
+                    <span v-if="description">{{ description }}</span>
+                    <slot v-else name="description" />
+                </p>
+            </div>
         </div>
 
         <div v-if="pageImage" class="pb-4 grid justify-center dark:opacity-70">
-            <img :src="pageImage" class=" w-[24vw] portrait:w-[45vw] max-w-72 h-[24vw] portrait:h-[45vw] max-h-72" />
+            <img
+                :src="pageImage"
+                class="w-[24vw] portrait:w-[45vw] max-w-72 h-[24vw] portrait:h-[45vw] max-h-72"
+            />
         </div>
         <slot />
     </div>
--- resources/views/auth/phone-verification.vue
+++ resources/views/auth/phone-verification.vue
@@ -26,7 +26,7 @@ const form = useForm({
         </template>
 
         <form @submit.prevent="form.submit">
-            <div     class="grid grid-flow-row portrait:grid-flow-row md:grid-flow-col gap-5 w-full">
+            <div     class="grid  gap-5 w-full">
                 <!-- <SharedFormBaseOtp :length="4" @updateOtp="handleOtpUpdate" /> -->
                 <SharedFormBaseOtpInput
                     v-model="form.fields.otp"
