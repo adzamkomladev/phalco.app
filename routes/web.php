7044f4b8e054b3430a9888540d340dcd65ad91e3
EveryJE <kgyan19lf@outlook.com>
2024-10-18 18:52:32 +0100

    auth:fixed layout alignment issues

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
 
 Route::get('/phone-verif', function () {
 
-    return hybridly('auth.reset-password');
+    return hybridly('auth.phone-verification');
 })->name('phone-verif');
 
 Route::get('/elections', function () {
