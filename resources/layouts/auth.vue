872d482457d026cff8e9e1b8fcc0246875637f1a
EveryJE <kgyan19lf@outlook.com>
2024-10-20 15:29:44 +0100

    chore: code formatted

--- resources/layouts/auth.vue
+++ resources/layouts/auth.vue
@@ -37,28 +37,29 @@ onMounted(() => {
             <div
                 class="pt-8 sm:landscape:pt-10 -mt px-[10%] sm:portrait:px-[14%] sm:landscape:mt-0 pb-10 sm:landscape:px-0 sm:landscape:pr-[5vw] xl:pr-[8vw] z-50 bg-white rounded-t-[2.5rem] sm:landscape:rounded-t-none flex-1 sm:landscape:overflow-y-auto sm:landscape:h-screen dark:bg-gray-800"
             >
-            <transition name="fade" mode="out-in">
-                                <slot />
-            </transition>
-          
+                <transition name="fade" mode="out-in">
+                    <slot />
+                </transition>
             </div>
         </section>
     </main>
 </template>
 <style scoped>
-main{}
+main {
+}
 
-.fade-enter-from,.fade-leave-to{
-opacity: 0;
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
 }
-.fade-enter-from{
-transform: skewX(30deg);
-
+.fade-enter-from {
+    transform: skewX(30deg);
 }
-.fade-leave-to{
-transform: skewX(-30deg);
+.fade-leave-to {
+    transform: skewX(-30deg);
 }
-.fade-enter-active,.fade-leave-active{
-    transition:all 0.2s ease-out;
+.fade-enter-active,
+.fade-leave-active {
+    transition: all 0.2s ease-out;
 }
 </style>
