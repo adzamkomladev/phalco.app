39eccb2dea5e4c59162e5c683e9a13786fbc449d
EveryJE <kgyan19lf@outlook.com>
2024-10-20 15:29:44 +0100

    chore:added vue motion plugin

--- resources/components/landing/header/menu/item.vue
+++ resources/components/landing/header/menu/item.vue
@@ -3,7 +3,7 @@ const props = defineProps<{ to: string }>();
 </script>
 <template>
     <a
-        class="text-base text-black hover:opacity-90 font-bold capitalize dark:text-white md:py-4 focus:outline-none focus:text-neutral-300"
+        class="text-base text-black hover:opacity-90 font-bold capitalize  md:py-4 focus:outline-none focus:text-neutral-300"
         :href="'#' + to"
         aria-current="page"
         >{{ to }}</a
