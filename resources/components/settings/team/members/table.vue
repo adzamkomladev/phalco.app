f0bec1145ed95123887214f17f6f720b6b0903a6
EveryJE <kgyan19lf@outlook.com>
2024-10-31 08:50:54 +0100

    feat: added a custom scrollbar

--- resources/components/settings/team/members/table.vue
+++ resources/components/settings/team/members/table.vue
@@ -54,7 +54,7 @@ watch(search, debounceFn);
                     <div>
                         <div class="inline-flex gap-x-2">
                             <router-link
-                                class="inline-flex items-center px-3 py-2 text-base font-semibold text-gray-400 bg-white border border-gray-200 rounded-md shadow-sm gap-x-2 hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                                class="inline-flex items-center px-3 py-2 text-base font-medium text-gray-400 bg-white border border-gray-200 rounded-md shadow-sm gap-x-2 hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                                 :href="route('settings.team.roles')"
                             >
                                 Roles
@@ -127,108 +127,13 @@ watch(search, debounceFn);
                 </thead>
 
                 <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
-                    <tr
-                        v-for="{ key, value, extra } in table.records"
-                        :key="key"
-                    >
-                        <td class="size-px whitespace-nowrap">
-                            <div class="py-3 ps-6">
-                                <label
-                                    for="hs-at-with-checkboxes-1"
-                                    class="flex"
-                                >
-                                    <input
-                                        type="checkbox"
-                                        class="text-primary-600 border-gray-300 rounded shrink-0 focus:ring-primary-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-600 dark:checked:bg-primary-500 dark:checked:border-primary-500 dark:focus:ring-offset-gray-800"
-                                        id="hs-at-with-checkboxes-1"
-                                    />
-                                    <span class="sr-only">Checkbox</span>
-                                </label>
-                            </div>
-                        </td>
-                        <td class="size-px whitespace-nowrap">
-                            <div class="py-3 ps-6 lg:ps-3 xl:ps-0 pe-6">
-                                <div class="flex items-center gap-x-3">
-                                    <img
-                                        class="inline-block size-[38px] rounded-full"
-                                        :src="
-                                            extra(findColumn('name'), 'avatar')
-                                        "
-                                        alt="Avatar"
-                                    />
-                                    <div class="grow">
-                                        <span
-                                            class="block text-sm font-semibold text-gray-800 dark:text-gray-200"
-                                            >{{
-                                                value(findColumn("name"))
-                                            }}</span
-                                        >
-                                        <span
-                                            class="block text-sm text-gray-500"
-                                        >
-                                            {{
-                                                extra(
-                                                    findColumn("name"),
-                                                    "email",
-                                                )
-                                            }}
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </td>
-                        <td class="size-px whitespace-nowrap">
-                            <div class="px-6 py-3">
-                                <span
-                                    class="block text-sm font-semibold text-gray-800 dark:text-gray-200 capitalize"
-                                >
-                                    {{ value(findColumn("position")) }}
-                                </span>
-                                <span class="block text-sm text-gray-500">
-                                    {{ extra(findColumn("position"), "role") }}
-                                </span>
-                            </div>
-                        </td>
-                        <td class="size-px whitespace-nowrap">
-                            <SharedTableStatus
-                                :status="value(findColumn('status'))"
-                            />
-                        </td>
-                        <td class="size-px whitespace-nowrap">
-                            <div class="px-6 py-3">
-                                <div class="flex items-center gap-x-3">
-                                    <span
-                                        class="text-sm text-gray-800 dark:text-gray-200"
-                                    >
-                                        {{
-                                            value(findColumn("last_seen")) ===
-                                            "online"
-                                                ? "Online"
-                                                : value(findColumn("last_seen"))
-                                        }}
-                                    </span>
-                                </div>
-                            </div>
-                        </td>
-                        <td class="size-px whitespace-nowrap">
-                            <div class="px-6 py-3">
-                                <span
-                                    class="text-sm font-bold text-gray-800 dark:text-gray-200"
-                                    >{{ value(findColumn("added_on")) }}
-                                </span>
-                            </div>
-                        </td>
-                        <td class="size-px whitespace-nowrap">
-                            <div class="px-6 py-1.5">
-                                <a
-                                    data-hs-overlay="#hs-overlay-create-user"
-                                    class="inline-flex items-center text-sm font-medium text-primary-600 cursor-pointer gap-x-1 decoration-2 hover:underline dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                                >
-                                    Edit
-                                </a>
-                            </div>
-                        </td>
-                    </tr>
+                     <SettingsTeamMembersRow
+                        v-for="row in table.records"
+                        :key="row.key"
+                        :row="row"
+                        :columns="table.columns"
+                        :findColumn="findColumn"
+                    />
                 </tbody>
             </table>
             <!-- End Table -->
