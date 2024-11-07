37790506903a9af337a70bb593d3334e9245636d
Komla Adzam <adzamkomla.dev@gmail.com>
2024-11-07 19:30:09 +0000

    feat: start work on withdrawal paystack

---
+++ app/Rules/Finance/AdequateBalance.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Rules\Finance;
+
+use Bavix\Wallet\Models\Wallet;
+use Closure;
+
+use Illuminate\Contracts\Validation\DataAwareRule;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class AdequateBalance implements DataAwareRule, ValidationRule
+{
+    /**
+     * All of the data under validation.
+     *
+     * @var array<string, mixed>
+     */
+    protected $data = [];
+
+    /**
+     * Set the data under validation.
+     *
+     * @param  array<string, mixed>  $data
+     */
+    public function setData(array $data): static
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string, ?string=): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $wallet = Wallet::find($this->data['wallet_id']);
+
+        if ($wallet->balanceInt < ($value * 100)) {
+            $fail('Insufficient wallet balance');
+        }
+    }
+}
