554860a71b77df86c8cb45344026c92e758475ab
EveryJE <kgyan19lf@outlook.com>
2024-11-07 15:56:18 +0100

    chore:added testing data

--- resources/data/elections/create.ts
+++ resources/data/elections/create.ts
@@ -4,41 +4,41 @@ export const defaultElectionStages: ElectionStageProps[] = [
     {
         title: "Vetting",
         date: {
-            start: new Date("2024-01-10T09:00:00"), 
+            start: new Date("2024-01-10T09:00:00"),
             end: new Date("2024-01-12T17:00:00"),
         },
     },
     {
         title: "Nominations",
         date: {
-            start: new Date("2024-01-15T09:00:00"), 
+            start: new Date("2024-01-15T09:00:00"),
             end: new Date("2024-01-18T17:00:00"),}
     },
     {
         title: "Campaigning",
         date: {
-            start: new Date("2024-01-20T09:00:00"), 
+            start: new Date("2024-01-20T09:00:00"),
             end: new Date("2024-02-10T17:00:00"),
         },
     },
     {
         title: "Voting",
         date: {
-            start: new Date("2024-09-15T08:00:00"), 
-            end: new Date("2024-12-30T18:00:00"), 
+            start: new Date("2024-09-15T08:00:00"),
+            end: new Date("2024-12-30T18:00:00"),
         },
     },
     {
         title: "Collation",
         date: {
-            start: new Date("2025-01-01T09:00:00"), 
-            end: new Date("2025-01-09T23:59:00"), 
+            start: new Date("2025-01-01T09:00:00"),
+            end: new Date("2025-01-09T23:59:00"),
     }},
     {
         title: "Result",
         date: {
-            start: new Date("2025-2-10T10:00:00"), 
-            end: new Date("2025-02-17T12:00:00"), 
+            start: new Date("2025-02-10T10:00:00"),
+            end: new Date("2025-02-17T12:00:00"),
         },
     },
 ];
