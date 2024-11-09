5221195e7b10dd8e6dd22a2a4fa7f44c402070bd
Komla Adzam <adzamkomla.dev@gmail.com>
2024-11-09 23:49:35 +0000

    fix: favicon issue

--- resources/application/root.blade.php
+++ resources/application/root.blade.php
@@ -49,15 +49,16 @@
     <link rel="apple-touch-icon" sizes="120x120" href="/res-favicon.png">
     <link rel="apple-touch-icon" sizes="144x144" href="/res-favicon.png">
     <link rel="apple-touch-icon" sizes="152x152" href="/res-favicon.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/res-favicon.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/rs-favicon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="96x96" href="/rs-favicon.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/rs-favicon.png">
-    <link rel="manifest" href="/site.webmanifest.json">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
+
 
     @vite
 </head>
