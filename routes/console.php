f15bad4f579307ee5329e2a86cabfc8a9e5f2427
Komla Adzam <adzamkomla.dev@gmail.com>
2024-11-11 17:23:16 +0000

    chore: code formatted

--- routes/console.php
+++ routes/console.php
@@ -13,8 +13,8 @@ Artisan::command('view-octane-tests', function () {
     logger('Start view octane tests');
 
     [$appName, $user] = Octane::concurrently([
-        fn() => Cache::store('octane')->get('application'),
-        fn() => Octane::table('users')->get('123'),
+        fn () => Cache::store('octane')->get('application'),
+        fn () => Octane::table('users')->get('123'),
     ]);
 
     logger('Data retrieved from octane cache and table: ', ['appName' => $appName, 'user' => $user]);
@@ -22,12 +22,12 @@ Artisan::command('view-octane-tests', function () {
     logger('End view octane tests');
 })->purpose('View Octane Tests')->everyMinute();
 
-Artisan::command('telescope:prune --hours=720', fn() => $this->info('Telescope data from 30 days ago deleted'))
+Artisan::command('telescope:prune --hours=720', fn () => $this->info('Telescope data from 30 days ago deleted'))
     ->daily();
-Artisan::command('horizon:snapshot', fn() => $this->info('Horizon snapshot'))->everyFiveMinutes();
+Artisan::command('horizon:snapshot', fn () => $this->info('Horizon snapshot'))->everyFiveMinutes();
 
 // Sort of quick fix for Pulse memory issues
 // TODO: Fix pulse:check memory issues properly
-Artisan::command('pulse:restart', fn() => $this->info('Pulse Restarted'))
+Artisan::command('pulse:restart', fn () => $this->info('Pulse Restarted'))
     ->purpose('Restart pulse to clear memory hogging situation')
     ->hourly();
