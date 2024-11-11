5a620c7486da971c3e412fb8d8deb43ea869692b
Komla Adzam <adzamkomla.dev@gmail.com>
2024-11-11 17:34:40 +0000

    fix: add removed fonts

--- resources/application/root.blade.php
+++ resources/application/root.blade.php
@@ -2,12 +2,11 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full flex flex-col scroll-smooth">
 
 <head>
-     <meta property="og:title" content="Phalco" />
-  <meta property="og:description" content="Simplify election management and engage your community seamlessly" />
-  <meta property="og:url" content="https://wwww.phalco.africa.com" />
-  <meta property="og:image" content="/android-chrome-192x192.png" />
-  <meta property="og:type" content="website" />
-</head>
+    <meta property="og:title" content="Phalco" />
+    <meta property="og:description" content="Simplify election management and engage your community seamlessly" />
+    <meta property="og:url" content="https://wwww.phalco.africa.com" />
+    <meta property="og:image" content="/android-chrome-192x192.png" />
+    <meta property="og:type" content="website" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -65,11 +64,16 @@
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
 
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&amp;display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet" />
 
     @vite
 </head>
 
-<body class="h-full bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-300 antialiased !scroll-smooth font-poppins">
+<body
+    class="h-full bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-300 antialiased !scroll-smooth font-poppins">
     @hybridly(class: 'h-full')
 </body>
 
