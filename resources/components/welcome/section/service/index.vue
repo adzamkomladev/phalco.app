fa3ca6cf6d6e6c08ccbcdecdbac0db126f1e9976
EveryJE <kgyan19lf@outlook.com>
2024-11-18 19:31:14 +0100

    chore: code formatted

--- resources/components/welcome/section/service/index.vue
+++ resources/components/welcome/section/service/index.vue
@@ -30,46 +30,9 @@ import ManagementImage from "~/resources/svg/welcome/management.webp?url";
             </span>
         </div>
 
-        <div class="grid odd:flex-col-reverse">
-            <div class="flex items-center">
-                <div class="px-[10%] basis-1/2 shrink-0">
-                    <img :src="FastDecisionImage" class="" />
-                </div>
-                <div>
-                    <p
-                        v-motion-fade-visible
-                        :duration="300"
-                        :delay="90"
-                        class="font-black font-sans text-3xl md:text-4xl pb-4"
-                    >
-                        Fast Decisions
-                    </p>
-                    <p
-                        v-motion-fade-visible
-                        :duration="360"
-                        :delay="90"
-                        class="text-gray-700 block self-center sm:pr-[20%] text-justify"
-                    >
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        Nisi architecto cupiditate aspernatur, laudantium sunt
-                        tempora ullam eaque impedit iste. Quia minus eum
-                        excepturi, labore nemo commodi officia magni possimus
-                        eligendi.
-                    </p>
-                    <routerlink
-                        class="text-secondary-500 group flex items-center gap-2 my-4"
-                    >
-                        <span class="text-base font-semibold">Learn More</span>
-                        <SharedCommonIcon
-                            name="arrow_farward"
-                            class="transition-all group:hover:transition-y-5 h-4"
-                        />
-                    </routerlink>
-                </div>
-            </div>
-            <div class="flex items-center">
-                <div class="px-[10%] basis-1/2 shrink-0">
+        <div class="grid">
+            <div class="grid items-center sm:grid-cols-2">
+                <div class="px-[20%]">
                     <img :src="FastDecisionImage" class="" />
                 </div>
                 <div>
