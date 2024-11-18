f474416b53dac4cf4894ef13cb11d683df091a74
Komla Adzam <adzamkomla.dev@gmail.com>
2024-11-18 17:25:00 +0000

    feat: update show data returned

--- app/Actions/Elections/Show.php
+++ app/Actions/Elections/Show.php
@@ -24,30 +24,30 @@ class Show
             ->find($electionId);
 
         $stage = $election->stages->first(
-            fn (ElectionStage $value, int $key) => $value->start < now() && $value->end >= now(),
+            fn(ElectionStage $value, int $key) => $value->start < now() && $value->end >= now(),
         );
 
         $stageStats = null;
 
         if ($stage?->stage === 'nominations') {
             $stageStats = [
-                'total' => 0,
-                'approved' => 0,
-                'successful' => 0,
+                'total' => 20,
+                'approved' => 10,
+                'successful' => 10,
                 'totalSpent' => 2000,
                 'pending' => 0,
             ];
         } elseif ($stage?->stage === 'voting') {
             $stageStats = [
-                'voters' => 0,
-                'pollingStations' => 0,
-                'ballots' => 0,
-                'candidates' => 0,
+                'voters' => 3000,
+                'pollingStations' => 230,
+                'ballots' => 200,
+                'candidates' => 10,
             ];
         } elseif ($stage?->stage === 'campaigns') {
             $stageStats = [
-                'campaigns' => 0,
-                'audiences' => 0,
+                'campaigns' => 10,
+                'audiences' => 40,
                 'message' => [
                     'id' => 1,
                     'text' => 'Hello world',
@@ -61,21 +61,21 @@ class Show
             ];
         } else {
             $stageStats = [
-                'totalBalance' => 0,
-                'totalDonations' => 0,
-                'totalNominations' => 0,
+                'totalBalance' => 4000,
+                'totalDonations' => 2300,
+                'totalNominations' => 20,
                 'spent' => [
                     'jan' => 0,
-                    'feb' => 0,
+                    'feb' => 110,
                     'mar' => 0,
-                    'apr' => 0,
-                    'may' => 0,
+                    'apr' => 230,
+                    'may' => 70,
                     'jun' => 0,
-                    'jul' => 0,
+                    'jul' => 90,
                     'aug' => 0,
-                    'sep' => 0,
+                    'sep' => 560,
                     'oct' => 0,
-                    'nov' => 0,
+                    'nov' => 450,
                     'dec' => 0,
                 ],
             ];
@@ -85,20 +85,20 @@ class Show
             'election' => $election,
             'stats' => [
                 'voters' => [
-                    'total' => 0,
-                    'voted' => 0,
+                    'total' => 20,
+                    'voted' => 10,
                 ],
                 'nominations' => [
-                    'submitted' => 0,
-                    'approved' => 0,
+                    'submitted' => 230,
+                    'approved' => 10,
                 ],
                 'pollingStations' => [
-                    'total' => 0,
-                    'active' => 0,
+                    'total' => 230,
+                    'active' => 220,
                 ],
                 'campaigns' => [
-                    'total' => 0,
-                    'active' => 0,
+                    'total' => 20,
+                    'active' => 3,
                 ],
             ],
             'stage' => $stage,
