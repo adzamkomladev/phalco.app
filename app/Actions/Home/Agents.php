68a9ae4aa82b419df94310e8c3c7fcec6faec82a
Komla Adzam <adzamkomla.dev@gmail.com>
2024-11-25 15:28:39 +0000

    feat: send data for agents dashboard

--- app/Actions/Home/Agents.php
+++ app/Actions/Home/Agents.php
@@ -2,6 +2,9 @@
 
 namespace App\Actions\Home;
 
+use App\Actions\Voting\Requests\Index;
+use App\Models\Ballot;
+use App\Models\VoteEntryRequest;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 class Agents
@@ -15,6 +18,33 @@ class Agents
 
     public function handle()
     {
-        // ...
+        [
+            'submitted' => $submitted,
+            'pending' => $pending,
+        ] =  Index::run(56, 5, 2);
+
+        $submitted = collect($submitted)
+            ->map(fn(VoteEntryRequest $request) => [
+                'id' => $request->id,
+                'position' => $request->ballot->position,
+                'description' => $request->ballot->description,
+                'status' => $request->status,
+                'created_at' => $request->created_at,
+            ]);
+
+        $pending = collect($pending)
+            ->map(fn(Ballot $ballot) => [
+                'id' => $ballot->id,
+                'position' => $ballot->position,
+                'description' => $ballot->description,
+                'status' => 'not_started',
+                'created_at' => null
+            ]);
+
+        $requests = $submitted->merge($pending);
+        return [
+            'requests' => $requests,
+            'agents' => [],
+        ];
     }
 }
