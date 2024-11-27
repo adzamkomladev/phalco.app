11a2a927ee967e9c3774dd266fd88c32b5cd9d12
EveryJE <kgyan19lf@outlook.com>
2024-11-27 03:22:52 +0100

    fix:feat/agent/message ui

--- resources/components/shared/common/chat-input.vue
+++ resources/components/shared/common/chat-input.vue
@@ -7,7 +7,7 @@ const model = defineModel();
         <input
             v-model="model"
             type="text"
-            class="py-3 px-5 pr-20 block w-full border-gray-200 rounded-full text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            class="py-3 px-5 pr-20 block w-full border-gray-200 bg-gray-200 blur-3xl  rounded-full text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
             placeholder="message"
         />
         <button
@@ -17,7 +17,7 @@ const model = defineModel();
                 name="kite"
                 class=""
                 :class="[
-                    model.trim() != '' ? 'text-primary-300' : 'text-[#C5BDBD]',
+                    model.trim() != '' ? 'text-primary-500' : 'text-[#C5BDBD]',
                 ]"
             />
         </button>
