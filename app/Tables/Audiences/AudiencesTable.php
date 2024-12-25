9097126642393d4500281400c71053c44ba10843
Komla Adzam <adzamkomla.dev@gmail.com>
2024-12-25 12:43:53 +0000

    fix: add count for contacts in audiences

--- app/Tables/Audiences/AudiencesTable.php
+++ app/Tables/Audiences/AudiencesTable.php
@@ -21,22 +21,21 @@ final class AudiencesTable extends Table
         return [
             Columns\TextColumn::make('id')->label('#')->visible(false),
             Columns\TextColumn::make('name')->label('Name')
-                ->extra((fn (Audience $audience) => [
+                ->extra((fn(Audience $audience) => [
                     'id' => $audience->id,
                 ])),
             Columns\TextColumn::make('created_by')
                 ->label('Created By')
-                ->transformValueUsing(fn (Audience $audience) => $audience->createdBy->name)
-                ->extra((fn (Audience $audience) => [
+                ->transformValueUsing(fn(Audience $audience) => $audience->createdBy->name)
+                ->extra((fn(Audience $audience) => [
                     'avatar' => $audience->createdBy->avatar,
                 ])),
             Columns\TextColumn::make('status')->label('Status'),
             Columns\TextColumn::make('total_contacts')->label('Total Contacts')
-            // ->transformValueUsing(fn (Audience $audience) => $audience->contacts_count),
-                ->transformValueUsing(fn (Audience $audience) => 0),
+            ->transformValueUsing(fn(Audience $audience) => $audience->contacts_count),
             Columns\TextColumn::make('description')->label('Description'),
             Columns\TextColumn::make('created_at')->label('Created')
-                ->transformValueUsing(fn (Audience $audience) => $audience->created_at->diffForHumans()),
+                ->transformValueUsing(fn(Audience $audience) => $audience->created_at->diffForHumans()),
         ];
     }
 
@@ -64,7 +63,7 @@ final class AudiencesTable extends Table
             ->query()
             ->select(['id', 'user_id', 'organization_id', 'name', 'status', 'description', 'created_at'])
             ->with(['createdBy:id,first_name,last_name,avatar,selected_organization_id'])
-            // ->withCount('contacts')
+            ->withCount('contacts')
             ->where('organization_id', $this->organizationId);
     }
 }
